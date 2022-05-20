0d757d4efc4dbb885b83a5d728fee5106b72e5e1
LDprg <71488985+LDprg@users.noreply.github.com>
2022-05-20 13:30:39 +0200

    Add html test

--- lib/SignUpScreen.dart
+++ lib/SignUpScreen.dart
@@ -1,27 +1,40 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:learnify/constants.dart';
 
 class SignUpScreen extends StatefulWidget {
+  const SignUpScreen({super.key});
+
   @override
   _SignUpScreen createState() => _SignUpScreen();
 }
 
 class _SignUpScreen extends State<SignUpScreen> {
-  int _count = 0;
+  late Future<http.Response> futureStr;
+
+  Future<http.Response> fetchStr() async{
+    return http.get(Uri.parse(apiUrl));
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    futureStr = fetchStr();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         backgroundColor: Colors.grey[200],
         body: Center(
-            child: ElevatedButton(
-              onPressed: () {
-                setState(() {
-                  _count++;
-                });
-              },
-              child: Text(_count.toString()),
-            )
-        )
-    );
+            child: FutureBuilder<http.Response>(
+                future: futureStr,
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    return Text(snapshot.data!.body.toString());
+                  }
+                  // By default, show a loading spinner.
+                  return const CircularProgressIndicator();
+                })));
   }
 }
---
+++ lib/constants.dart
@@ -0,0 +1,2 @@
+
+const String apiUrl="http://127.0.0.1";
